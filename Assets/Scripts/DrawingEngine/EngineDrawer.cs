082a4713237120125a86014af79138c91d27efd9
Aurcereal <aurkius@gmail.com>
2023-05-15 16:56:03 -0700

    Update
    ?

---
+++ Assets/Scripts/DrawingEngine/EngineDrawer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class EngineDrawer
+{
+
+    public EngineDrawer(Camera camera)
+    {
+
+        //argsBuffer = new uint[5]
+        //{
+
+        //};
+
+
+    }
+
+    uint[] argsBuffer;
+
+    public void UpdateBuffers()
+    {
+
+
+
+    }
+
+    public void Draw()
+    {
+
+    }
+
+}
