a623235dc167f7c546b55d9ca425b4d83af0343f
Aurcereal <aurkius@gmail.com>
2023-05-16 19:03:04 -0700

    Update
    ?????????????

--- Assets/Scripts/Game Player/Strategy/Strategy.cs
+++ Assets/Scripts/Game Player/Strategy/Strategy.cs
@@ -18,19 +18,24 @@ public class BuilderMove
     public BuilderMove(Node n1, Node n2)
     { this.n1 = n1; this.n2 = n2; }
 
-    public void MakeMove(GraphManager graph)
-        => graph.CreateEdge(n1, n2, -1);
+    public void MakeMove(BoardManager board)
+        => board.CreateEdge(n1, n2, -1);
 
 }
 
 public class PainterMove
 {
+    Edge edge; int type;
+    public PainterMove(Edge edge, int type)
+    { this.edge = edge; this.type = type; }
 
+    public void MakeMove(BoardManager board)
+        => board.PaintEdge(edge, type);
 }
 
 public static class GameAlgorithms
 {
 
-    //Methods take in IGraphView
+    //Methods take in IGraphView return a bool or something
 
 }
