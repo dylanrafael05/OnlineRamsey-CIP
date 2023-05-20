e39aa054ca8b902e338496d4e817b0d003cc4a01
Aurcereal <aurkius@gmail.com>
2023-05-20 15:37:40 -0700

    85

--- Assets/Scripts/Gameplayer/User Player/UserPlayer.cs
+++ Assets/Scripts/Gameplayer/User Player/UserPlayer.cs
@@ -1,28 +1,74 @@
 using Ramsey.Graph;
 using System.Threading.Tasks;
 using Ramsey.Utilities;
+using Ramsey.Board;
+using System.Linq;
+using Unity.Mathematics;
+using static Unity.Mathematics.math;
 
 namespace Ramsey.Gameplayer
 {
-    public class UserBuilder : IBuilder
+    public class UserBuilder : IBuilder, IUserMode
     {
         public async Task<BuilderMove> GetMove(GameState gameState)
         {
-            await Utils.WaitUntil(() => UserInteraction.Ins.CurrNode != null && UserInteraction.Ins.PrevNode != null, 10);
+            UserModeHandler.AddMode(this);
+            await Utils.WaitUntil(() => currNode != null && prevNode != null, 10);
+            UserModeHandler.DelMode(this);
 
-            return new BuilderMove(UserInteraction.Ins.CurrNode, UserInteraction.Ins.PrevNode);
+            return new BuilderMove(currNode, prevNode);
         }
+
+        //
+        Node prevNode;
+        Node currNode;
+        bool CollideNode(float2 mouse, Node n, BoardManager board)
+            => math.length(mouse - n.Position) <= board.Preferences.drawingPreferences.nodeRadius;
+
+        public void Init() => prevNode = currNode = null;
+        public void Update(InputData input, BoardManager board)
+        {
+            if (input.lmbp)
+            {
+                prevNode = currNode;
+                currNode = board.Nodes.FirstOrDefault(n => CollideNode(input.mouse, n, board));
+            }
+        }
+        public void End() { }
 
     }
 
-    public class UserPainter : IPainter
+    public class UserPainter : IPainter, IUserMode
     {
         public async Task<PainterMove> GetMove(GameState gameState)
         {
-            await Utils.WaitUntil(() => UserInteraction.Ins.CurrEdge != null, 10);
+            UserModeHandler.AddMode(this);
+            await Utils.WaitUntil(() => currEdge != null, 10);
+            UserModeHandler.DelMode(this);
 
-            return new PainterMove(UserInteraction.Ins.CurrEdge, UserInteraction.Ins.CurrEdgeType);
+            return new PainterMove(currEdge, currEdgeType);
         }
+
+        //
+        Edge currEdge;
+        int currEdgeType;
+        bool CollideEdge(float2 mouse, Edge e, BoardManager board)
+        {
+            float2 StartToEnd = e.End.Position - e.Start.Position; float2 lp = mouse - .5f * (e.Start.Position + e.End.Position); float2 dir = normalize(StartToEnd);
+            lp = new float2(dot(lp, dir), length(cross(float3(lp, 0f), float3(dir, 0f)))); lp.x = abs(lp.x);
+            return lp.x <= length(StartToEnd) * .5f && lp.y <= board.Preferences.drawingPreferences.edgeThickness * .5f;
+        }
+
+        public void Init() => currEdge = null;
+        public void Update(InputData input, BoardManager board)
+        {
+            if(input.rmbp || input.lmbp)
+            {
+                currEdge = board.Edges.FirstOrDefault(e => CollideEdge(input.mouse, e, board));
+                currEdgeType = input.lmbp ? 0 : 1;
+            }
+        }
+        public void End() { }
     }
 
 }
---
+++ Assets/Scripts/UI/Modes/UserModeHandler.cs
@@ -0,0 +1,33 @@
+﻿using Ramsey.Board;
+using System.Collections.Generic;
+
+public class UserModeHandler
+{
+
+    static List<IUserMode> currentModes;
+
+    public static void Update(InputData input, BoardManager board)
+        => currentModes.ForEach(m => m.Update(input, board));
+
+    public static void AddMode(IUserMode mode)
+    {
+        currentModes.Add(mode);
+        mode.Init();
+    }
+
+    public static void DelMode(IUserMode mode)
+    {
+        currentModes.Remove(mode);
+        mode.End();
+    }
+
+}
+
+public interface IUserMode
+{
+
+    void Init();
+    void Update(InputData input, BoardManager board);
+    void End();
+
+}
