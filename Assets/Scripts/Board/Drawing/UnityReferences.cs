b4aeed5516d8bda882d2fe4985386507befe423f
Dylan Rafael <dylanrafael05@gmail.com>
2023-06-14 18:15:04 -0400

    Update UnityReferences.cs

--- Assets/Scripts/Board/Drawing/UnityReferences.cs
+++ Assets/Scripts/Board/Drawing/UnityReferences.cs
@@ -31,8 +31,8 @@ namespace Ramsey.Drawing
             var lgo = GameObject.Find("Loading");
             LoadingTransform = lgo.GetComponent<RectTransform>();
 
-            var wso = GameObject.Find("WheelSelect");
-            WheelSelectTransform = wso.GetComponent<RectTransform>();
+            // var wso = GameObject.Find("WheelSelect");
+            // WheelSelectTransform = wso.GetComponent<RectTransform>();
 
             GoalText = GameObject.Find("Goal Text").GetComponent<Text>();
             TurnText = GameObject.Find("Turn Text").GetComponent<Text>();
@@ -40,7 +40,7 @@ namespace Ramsey.Drawing
 
             OverText.gameObject.SetActive(false);
 
-            EdgeMaterial = new(Shader.Find("Unlit/GraphShaders/EdgeShader")) { enableInstancing = true };
+            EdgeMaterial = new(Shader.Find("Unlit/GraphShaders/EdgeShader"));
             NodeMaterial = new(Shader.Find("Unlit/GraphShaders/NodeShader"));
             RecorderMaterial = new(Shader.Find("Unlit/UIShaders/RecordingShader"));
             LoadingMaterial = new(Shader.Find("Unlit/UIShaders/LoadingCircle"));
